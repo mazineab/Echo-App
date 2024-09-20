0e6b6b55fb5746fa603bf0c7e44dbb3e9a1b02ea
mazinabjlal <mazinabjlal@gmail.com>
2024-09-20 23:26:46 +0000

    update models

--- lib/data/models/comment.dart
+++ lib/data/models/comment.dart
@@ -3,6 +3,8 @@ class Comment {
   String userId;
   String content;
   String statusId;
+  String? userFullName;
+  String? createAt;
   // List<Comment>? listComments;
 
   Comment(
@@ -10,15 +12,19 @@ class Comment {
       required this.userId,
       required this.content,
       required this.statusId,
+      required this.userFullName,
+      required this.createAt
       // this.listComments
       });
 
   factory Comment.fromJson(Map<String, dynamic> data) {
     return Comment(
-        id: data['id']??'',
-        userId: data['userId']??'',
-        content: data['content']??'',
-        statusId: data['statusId']??''
+        id: data['id'] ?? '',
+        userId: data['userId'] ?? '',
+        content: data['content'] ?? '',
+        statusId: data['statusId'] ?? '',
+        userFullName: data['userFullName'] ?? '',
+        createAt:data['createAt'] ?? ''
         // listComment: data['listComment'] ?? <Comment>[]
         // listComments: data['listComments'] !=null
         // ?(data['listComments'] as List<Comment>).map((e)=>Comment.fromJson(e as Map<String,dynamic>)).toList()
@@ -30,9 +36,11 @@ class Comment {
     return {
       'id': id,
       'userId': userId,
-      'statusId':statusId,
+      'statusId': statusId,
       'content': content,
+      'userFullName': userFullName,
+      'createAt':createAt
       // 'listComment': listComments,
     };
-  } 
+  }
 }
--- lib/data/models/status.dart
+++ lib/data/models/status.dart
@@ -1,3 +1,4 @@
+import 'package:intl/intl.dart';
 import 'package:myapp/data/models/comment.dart';
 import 'package:myapp/data/models/like.dart';
 import 'package:myapp/data/models/tag.dart';
@@ -6,10 +7,12 @@ class Status {
   String? id;
   String content;
   String userId;
+  String? fullUserName;
   List<Tag> listTags;
   String? commentCount;
   List<Like>? listLikes;
   List<Comment>? listComments;
+  String? createAt;
 
   Status(
       {this.id,
@@ -17,7 +20,9 @@ class Status {
       required this.content,
       required this.listTags,
       required this.commentCount,
+      required this.fullUserName,
       required this.listComments,
+      required this.createAt,
       required this.listLikes});
 
   factory Status.fromJson(Map<String, dynamic> data) {
@@ -30,12 +35,14 @@ class Status {
                 .map((e) => Tag.fromJson(e as Map<String, dynamic>))
                 .toList()
             : <Tag>[],
-        commentCount: data['commentsCount'] ??'',
+        fullUserName: data['fullUserName'] ?? '',
+        commentCount: data['commentsCount'] ?? '',
         listComments: data['listComments'] != null
             ? (data['listComments'] as List<dynamic>)
                 .map((e) => Comment.fromJson(e as Map<String, dynamic>))
                 .toList()
             : <Comment>[],
+        createAt: data['createAt'] ?? '',
         listLikes: data['listLikes'] != null
             ? (data['listLikes'] as List<dynamic>)
                 .map((e) => Like.fromJson(e as Map<String, dynamic>))
@@ -50,8 +57,28 @@ class Status {
       'content': content,
       'listTags': listTags,
       'listComments': listComments,
-      'commentsCount': commentCount??'',
+      'commentsCount': commentCount ?? '',
+      'fullUserName': fullUserName ?? '',
       'listLikes': listLikes,
+      'createAt': createAt
     };
   }
+
+  formateDate() {
+    try {
+      DateTime parseDate = DateTime.parse(createAt ?? '');
+      return DateFormat("dd/MM/yyyy hh:mm").format(parseDate);
+    } catch (e) {
+      Exception(e);
+      return '';
+    }
+  }
+
+  // List<Comment> sortedComments() {
+  //   try{
+  //     return listComments!.sort((a, b) => b.createAt!.compareTo(a.createAt!));
+  //   }catch(e){
+  //     Exception(e);
+  //   }
+  // }
 }
